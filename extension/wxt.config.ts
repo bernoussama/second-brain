3555f69cbdb2fa6526a00c1242483df8ae3b9442
bernoussama <oussama.bernou@outlook.com>
2025-05-14 22:07:02 +0100

    Add Tailwind CSS and shadcn configuration and global styles, update component paths, and implement utility functions

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite'
 // See https://wxt.dev/api/config.html
@@ -5,6 +6,11 @@ export default defineConfig({
   // @ts-ignore
   vite: () => ({
     plugins: [tailwindcss()],
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
   }),
   modules: ['@wxt-dev/module-react'],
 });
